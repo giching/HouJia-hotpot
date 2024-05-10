182c168081e33749763a7786df4cb15bc7b5272e
Giching <wendy57372@gmail.com>
2024-05-10 15:07:41 +0800

    Initial commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,83 @@
+<script>
+import logo from '@/assets/houjialogo.png'
+import { RouterLink } from 'vue-router'
+
+export default{
+    data() {
+        return{
+            logo,
+        }
+    },
+}
+</script>
+<template>
+    <div class="nav-bar-all">
+        <div class="row flex justify-between">
+            <!-- Nav Logo -->
+            <section class="">
+                <div id="nav-img">
+                    <a class="" href=""><img class="logo" :src=logo alt="HouJia-logo"></a>
+                </div>
+            </section>
+            <section class="w-full border-4 border-pink-200 text-end">
+                <!-- <RouterLink to="/">Contact</RouterLink> -->
+                <!-- <RouterLink to="/">News</RouterLink>
+                <RouterLink to="about">Menu</RouterLink> -->
+                <div class="">
+                    <!-- <nav class="navbar navbar-expand-md">
+                        <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                            data-bs-target="#navDropdown">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div class="collapse navbar-collapse" id="navDropdown">
+                            <ul class="navbar-nav">
+                                <li class="nav-item"><a class="nav-link" href="">News</a></li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown">Menu</a>
+                                    <div class="dropdown-menu">
+                                        <a class="dropdown-item" href="">Soup</a>
+                                        <a class="dropdown-item" href="">Ingredient</a>
+                                        <a class="dropdown-item" href="">Side</a>
+                                        <a class="dropdown-item" href="">Drink</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item"><a class="nav-link" href="">Contact</a></li>
+                                <li class="nav-item"><a class="nav-link" href="">Reservation</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav> -->
+                </div>
+            </section>
+        </div>
+    </div>
+</template>
+<style scoped>
+.nav-bar-all{
+    width: 100%;
+    background-color: rgba(255, 199, 135, 0.637);
+    padding: 10px;
+    backdrop-filter: blur(5px);
+}
+.row{
+    padding: 10px;
+    margin: 0;
+    /* display: flex; */
+}
+#nav-img{
+    border: 2px solid yellow;
+    /* height:50px; */
+    width:100%;
+}
+img{
+    height:10vh;
+    width: auto;
+}
+.navbar-nav{
+    width: 100%;
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    gap: 12px;
+}
+</style>
---
+++ src/views/Index.vue
@@ -0,0 +1,121 @@
+<script>
+import NavBar from '@/components/NavBar.vue'
+import hotpotAnimation from '@/assets/Animated-Logo.mp4'
+// core version + navigation, pagination modules:
+// import Swiper from 'swiper'
+// // import Swiper and modules styles
+// import 'swiper/css'
+// import 'swiper/css/navigation'
+// import 'swiper/css/pagination'
+
+export default {
+  name: 'HouJia Hot Pot',
+  components: {
+    NavBar,
+  },
+  data () {
+    return {
+      hotpotAnimation
+    }
+  },
+  mounted () {
+    window.addEventListener('scroll', this.handleScroll)
+    // this.bannerSwiper()
+  },
+
+  beforeUnmount () {
+    window.removeEventListener('scroll', this.handleScroll)
+  },
+  methods: {
+    handleScroll () {
+      const video = this.$refs.videoRef
+      const viewportHeight = window.innerHeight
+      const scrollY = window.scrollY
+      if (scrollY <= viewportHeight) {
+        video.play()
+      } else {
+        video.pause()
+      }
+    },
+    // bannerSwiper () {
+    //   new Swiper('.swiper', {
+    //   // Optional parameters
+    //     direction: 'vertical',
+    //     loop: true,
+
+    //     // If we need pagination
+    //     pagination: {
+    //       el: '.swiper-pagination'
+    //     },
+
+    //     // Navigation arrows
+    //     navigation: {
+    //       nextEl: '.swiper-button-next',
+    //       prevEl: '.swiper-button-prev'
+    //     },
+
+    //     // And if we need scrollbar
+    //     scrollbar: {
+    //       el: '.swiper-scrollbar'
+    //     }
+    //   })
+    // }
+
+  }
+}
+</script>
+
+<template>
+
+    <body>
+        <video ref="videoRef" autoplay muted loop>
+            <source :src="hotpotAnimation" type="video/mp4">
+        </video>
+        <NavBar class="sticky top-0 z-30" />
+        <div class="row banner-section">
+            <div class="banner-all">
+                <!-- Slider main container -->
+                <!-- <Swiper class="swiper">
+                    <div class="swiper-wrapper">
+                        <div class="swiper-slide">Slide 1</div>
+                        <div class="swiper-slide">Slide 2</div>
+                        <div class="swiper-slide">Slide 3</div>
+                        ...
+                    </div>
+                    <div class="swiper-pagination"></div>
+
+                    <div class="swiper-button-prev"></div>
+                    <div class="swiper-button-next"></div>
+
+                    <div class="swiper-scrollbar"></div>
+                </Swiper> -->
+            </div>
+        </div>
+    </body>
+</template>
+<style scoped>
+.animation {
+    width: 100;
+    height: 500px;
+}
+
+.banner-section {
+    background-color: cadetblue;
+    height: 100vh;
+    position: relative;
+}
+
+.banner-all {
+    height: 600px;
+    width: 60%;
+    background-color: pink;
+    position: absolute;
+}
+
+video {
+    width: 100%;
+    height: 100vh;
+    object-fit: cover;
+    object-position: 75%;
+}
+</style>
